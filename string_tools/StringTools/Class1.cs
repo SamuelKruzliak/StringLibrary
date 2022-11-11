3e113b57a27eb78c032b71cbb80184f756406909
SamuelKruzliak <99512834+SamuelKruzliak@users.noreply.github.com>
2022-11-11 16:40:15 +0100

    Add files via upload

---
+++ string_tools/StringTools/Class1.cs
@@ -0,0 +1,44 @@
+﻿
+namespace StringTools
+{
+    public class ArrayLib
+    {
+        static public bool Contains(int[] array, int a){
+            foreach(int item in array){
+                if(item == a){
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        static public bool Contains(string[] array, string a){
+            foreach(string item in array){
+                if(item == a){
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        static public bool Contains(char[] array, char a){
+            foreach(char item in array){
+                if(item == a){
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        static public bool Contains(double[] array, double a){
+            foreach(double item in array){
+                if(item == a){
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+}
